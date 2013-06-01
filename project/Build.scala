7c4125f1b7e2e2e13e5c36c0f850ee15ccef48f5
Hannes Stockner <hannes.stockner@gmail.com>
2013-06-01 12:08:36 +0100

    Add bitcoinj dependency.

--- project/Build.scala
+++ project/Build.scala
@@ -11,11 +11,14 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
     javaCore,
     javaJdbc,
-    javaEbean
+    javaEbean,
+    "com.google" % "bitcoinj" % "0.8"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here 
+    resolvers += "mvnrepository" at "http://repo1.maven.org/maven2/",
+    resolvers += "Local Maven Repository" at "file://" + Path.userHome.absolutePath + "/.m2/repository"     
   )
 
 }
